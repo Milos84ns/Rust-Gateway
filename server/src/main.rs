c83df9c6a2d67e8de11011585683dc98dfe3f171
Milos <milos84ns@gmail.com>
2023-12-17 22:30:53 +0800

    simple server

---
+++ server/src/main.rs
@@ -0,0 +1,53 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(handle_request))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
+
+async fn handle_request(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+//    let res = reqwest::get("https://hyper.rs".into()).await?;
+
+    match req.uri().path() {
+        "/service1" => Ok(Response::new(Full::new(Bytes::from("Service 1")))),
+        "/service2" => Ok(Response::new(Full::new(Bytes::from("Service 2")))),
+      //  "/hyper" => Ok(Response::new(Full::new(Bytes::from(res)))),
+        _ => Ok(Response::new(Full::new(Bytes::from("No Service!")))),
+    }
+}
